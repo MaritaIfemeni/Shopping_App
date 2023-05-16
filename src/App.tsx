4fe9ea1746f091444aec11528bcd32edcfb09d82
MaritaIfemeni <marita.ifemeni@gmail.com>
2023-05-16 15:24:00 +0300

    moved router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 
-import routes from "./components/routes";
+import routes from "./routes/routes";
 import "./app.css";
 
 const App = () => {
